a10202336e3d8d4760309af9d301b9823e68572c
s-abe <s-abe@creationline.com>
2021-02-21 19:22:39 +0900

    画質いいやつ欲しいね

--- src/get_pic_from_google.py
+++ src/get_pic_from_google.py
@@ -1,4 +1,5 @@
 import requests
+import urllib.request
 import bs4
 import lxml
 
@@ -8,4 +9,15 @@ def get_image_link(keyword):
     soup = bs4.BeautifulSoup(req.text, 'lxml')
     link = soup.find_all("img")[1]
     pic = link['src']
+
+    # x = []
+    # for item in soup.find_all(attrs={"data-id":True}):
+    #     x.append(item['data-id'])
+    # print(x)
+
     return pic
+
+        
+
+## メモ
+# 画像urlを開くと画像検索url + #imgrc={data-id}　となるのでなんとかdata-idをとればurlに直接飛べるのでは
